c47c054aff93485bbe7db07a8d1ed1b374259662
云飞扬 <fan.huang@ideacome.com>
2016-12-01 18:51:30 +0800

    Build Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# SmartUtil
-Java一些工具类的集合
---
+++ src/main/java/com/smartutil/SmartUtilApplication.java
@@ -0,0 +1,12 @@
+package com.smartutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartUtilApplication.class, args);
+	}
+}
